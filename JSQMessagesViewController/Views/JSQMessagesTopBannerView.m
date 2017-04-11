1211bb79cd42f52f43b7219faf36164265e5aa9f
Besat Zardosht <besat@morgiij.com>
2017-04-11 10:06:35 -0400

    Top banner view height

--- JSQMessagesViewController/Views/JSQMessagesTopBannerView.m
+++ JSQMessagesViewController/Views/JSQMessagesTopBannerView.m
@@ -11,7 +11,6 @@
 
 @interface JSQMessagesTopBannerView ()
 
-@property (weak, nonatomic) IBOutlet UILabel *topBannerLabel;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *topBannerViewTopConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *topBannerViewHeightConstraint;
 
@@ -26,23 +25,11 @@
     [self setNeedsUpdateConstraints];
 }
 
--(void)resizeForWidth:(CGFloat)width
+-(void)setTopBannerViewHeight:(CGFloat)topBannerViewHeight
 {
-    CGSize size = [self.topBannerLabel sizeThatFits:CGSizeMake(width - 40, CGFLOAT_MAX)];
-    self.topBannerViewHeightConstraint.constant = size.height + 44.0f;
+    _topBannerViewHeight = topBannerViewHeight;
+    self.topBannerViewHeightConstraint.constant = topBannerViewHeight;
     [self setNeedsUpdateConstraints];
 }
-
--(void)setText:(NSString *)motdText
-{
-    self.topBannerLabel.text = motdText;
-    [self resizeForWidth:self.bounds.size.width];
-}
-
--(CGFloat)topBannerHeight
-{
-    return self.topBannerViewHeightConstraint.constant;
-}
-
 
 @end

25fad60e81780204d35b697b537bac84596ab99f
Jake Stephens <jake@binnj.com>
2020-09-22 14:39:22 -0400

    fix Xcode 12 / iOS 14 bug where media message previews werent showing
    - masking via UIImageView layer appears to have been broken, but we can still build the layer ourselves and use that

--- JSQMessagesViewController/Factories/JSQMessagesMediaViewBubbleImageMasker.m
+++ JSQMessagesViewController/Factories/JSQMessagesMediaViewBubbleImageMasker.m
@@ -81,10 +81,37 @@
     NSParameterAssert(view != nil);
     NSParameterAssert(image != nil);
     
+    /*
     UIImageView *imageViewMask = [[UIImageView alloc] initWithImage:image];
     imageViewMask.frame = CGRectInset(view.frame, 2.0f, 2.0f);
     
     view.layer.mask = imageViewMask.layer;
+    */
+     
+    //the original method of doing this is broken in Xcode 12/iOS 14; masking with UIImageView layers doesn't work anymore
+    //we can get around this by creating the mask layer by hand, but it's a little tricky:
+    
+    //first we need to get a new UIImage with the .orientation property reset to .up
+    //if we use the image as-is we lose the orientation of the UIImage when we grab its GCImage below
+    UIGraphicsBeginImageContextWithOptions(image.size, NO, image.scale);
+    [image drawAtPoint:CGPointMake(0, 0)];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    //create the layer that will be used as the mask
+    CALayer *layer = [[CALayer alloc] init];
+    layer.contents = (__bridge id _Nullable)(img.CGImage);
+    layer.contentsScale = img.scale;
+    
+    //the following line essentially nine-slices the message bubble shape so that it can be resized
+    layer.contentsCenter = CGRectMake((img.size.width / 2.0f - 1.0f) / img.size.width,
+                                      (img.size.height / 2.0f - 1.0f) / img.size.height,
+                                      1.0f / img.size.width,
+                                      1.0f / img.size.height);
+    
+    layer.frame = CGRectInset(view.frame, 2.0f, 2.0f);
+    
+    view.layer.mask = layer;
 }
 
 @end
